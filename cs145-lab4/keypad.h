b04f3333f915068b13cb977b5f9436f36e7b0973
yuntaz2 <yuntaz2@uci.edu>
2022-05-16 16:01:04 -0700

    Try to use Port C for LCD and Port A 0 for voltage

---
+++ cs145-lab4/keypad.h
@@ -0,0 +1,9 @@
+/*
+ * keypad.h
+ *
+ * Created: 5/16/2022 5:03:11 PM
+ *  Author: Yun
+ */ 
+unsigned char is_pressed(int r, int c);
+unsigned char get_key(void);
+unsigned mapping(unsigned char i);
