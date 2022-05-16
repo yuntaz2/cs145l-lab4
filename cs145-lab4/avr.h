3e6c3c780ddec3f9782f756d465195162048b92e
yuntaz2 <yuntaz2@uci.edu>
2022-05-16 15:49:30 -0700

    Try to use portA0

---
+++ cs145-lab4/avr.h
@@ -0,0 +1,26 @@
+/*
+ * avr.h
+ *
+ * Created: 4/4/2022 5:37:26 PM
+ *  Author: Yun
+ */ 
+
+
+#ifndef AVR_H_
+#define AVR_H_
+
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/io.h>
+
+#define XTAL_FRQ 8000000lu
+
+#define SET_BIT(p,i) ((p) |=  (1 << (i)))
+#define CLR_BIT(p,i) ((p) &= ~(1 << (i)))
+#define GET_BIT(p,i) ((p) &   (1 << (i)))
+
+#define NOP() asm volatile("nop"::)
+
+void avr_wait(unsigned short msec);
+
+#endif /* AVR_H_ */
